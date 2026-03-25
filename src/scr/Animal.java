bed56e7d340ac47cbb49b9c3419ca5deb38584c2
Renan Pacheco João <renan.pacheco@valtecinstalacao.com.br>
2026-03-25 18:03:12 -0300

    Primeiro commit

---
+++ src/scr/Animal.java
@@ -0,0 +1,106 @@
+package scr;
+
+public class Animal {
+	
+	private int codigo;
+	private String nome;
+	private String especie;
+	private String raca;
+	private int idade;
+	private double peso;
+	Dono dono;//Ligação com o dono
+	
+	//Construtor Padrão
+	public Animal() {
+		this.setCodigo(0);
+		this.setNome("");
+		this.setEspecie("");
+		this.setRaca("");
+		this.setIdade(0);
+		this.setPeso(0.0);
+		this.setDono(dono);
+	}//fim do construtor padrão
+	
+	//Construtor Animal
+	public Animal(int codigo, String nome, String especie, String raca, int idade, double peso, Dono dono) {
+	this.setCodigo(codigo);
+	this.setNome(nome);
+	this.setEspecie(especie);
+	this.setRaca(raca);
+	this.setIdade(idade);
+	this.setPeso(peso);
+	this.setDono(dono);
+	}//Fim do construtor Animal
+	
+	
+	
+	
+
+	public int getCodigo() {
+		return codigo;
+	}//Fim do get Codigo
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}//Fim do set codigo
+
+	public String getNome() {
+		return nome;
+	}//Fim do get Nome
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}//Fim do set Nome
+
+	public String getEspecie() {
+		return especie;
+	}//Fim do get Especie
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}//Fim do set Especie
+
+	public String getRaca() {
+		return raca;
+	}//Fim do get Raca
+
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}//Fim do set Raca
+
+	public int getIdade() {
+		return idade;
+	}//Fim do get Idade
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}//Fim do set Idade
+	
+	public double getPeso() {
+		return peso;
+	}//Fim do get Peso
+	
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}//Fim do get Peso
+	
+	public Dono getDono() {
+		return dono;
+	}//Fim do get Dono
+
+	public void setDono(Dono dono) {
+		this.dono = dono;
+	}//Fim do set Dono
+	
+	
+	//Método imprimir 
+	public String imprimir() {
+		return "Código: "  		+ getCodigo()  	+
+				"\nNome: "		+ getNome()		+
+				"\nEspécie: " 	+ getEspecie() 	+
+				"\nRaça: "		+ getRaca()     +
+				"\nIdade: "		+ getIdade()	+
+				"\nPeso: "		+ getPeso()		+
+				"\n---- Dono ----\n" + this.dono.imprimir();
+	}//fim do método imprimir 
+}
---
+++ src/scr/Dono.java
@@ -0,0 +1,80 @@
+package scr;
+
+public class Dono {
+	//1º Máxima do 0.0 = Encapsulamento
+	private int codigo;
+	private long cpf;
+	private String nome;
+	private String telefone; 
+	private String email;
+	Endereco endereco;
+	
+	public Dono() {
+		this.setCodigo (0);
+		this.setCPF(0);
+		this.setNome  ("");
+		this.setTelefone("");
+		this.setEmail ("");
+		this.setEndereco  (new Endereco());//Chamando construtor vazio - com valores padrões 
+	}//fim do construtor padrão
+	
+	//2º Máxima da O.O = Sopbrecarga
+	public Dono(int codigo, long cpf, String nome, String telefone, String email, Endereco endereco) {
+		this.setCodigo (codigo);
+		this.setCPF (cpf);
+		this.setNome (nome);
+		this.setTelefone (telefone);
+		this.setEmail  (email);
+		this.setEndereco (endereco);//Logradouro, numero, bairro, cidade, estado, UF, CEP
+	}//fim do construtor pessoa
+	
+	//Métodos modificadores
+	public int getCodigo() {
+		return this.codigo;
+	}//fim do getcodigo
+	public long getCPF() {
+		return this.cpf;
+	}//fim do getTelefone
+	public String getNome() {
+		return this.nome;
+	}//fim do getTelefone
+	public String getTelefone() {
+		return this.telefone;
+	}//fim do getTelefone
+	public String getEmail() {
+		return this.email;
+	}//fim do getEmail
+	
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}// fim do setCodigo
+	public void setCPF(long cpf) {
+		this.cpf = cpf;
+	}//fim do setCPF
+	public void setNome(String nome){
+		this.nome = nome;
+	}//fim do setNome
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}// fim do setTelefone
+	public void setEmail(String email) {
+		this.email = email;
+	}//fim do setEmail
+	
+	public Endereco getEndereco() {
+		return this.endereco;
+	}//fim do getEndereco
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}//fim do setEndereco 
+	
+	public String imprimir() {
+		return "Codigo: " 		+ getCodigo() 	+
+				"\nCPF: " 		+ getCPF() 		+
+				"\nNome: " 		+ getNome()		+
+				"\nTelefone: " 	+ getTelefone() +
+				"\nEmail: " 	+ getEmail()    +
+				"\nDados Endereço: " 			+ this.endereco.imprimir();
+	}//fim do imprimir
+	
+}
